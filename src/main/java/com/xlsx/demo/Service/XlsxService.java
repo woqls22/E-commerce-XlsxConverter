cd92b99d42e61d0f0e27a310d6b371bf882a355f
jaebeen_lee <jaebeen_lee@tmax.co.kr>
2021-12-01 10:33:48 +0900

    [Enhancement] xlsx Formula Applied

--- src/main/java/com/xlsx/demo/Service/XlsxService.java
+++ src/main/java/com/xlsx/demo/Service/XlsxService.java
@@ -19,6 +19,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 public class XlsxService {
@@ -46,7 +48,7 @@ public class XlsxService {
     XSSFSheet sheet = sourceWorkbook.getSheetAt(0);
     int rows = sheet.getPhysicalNumberOfRows(); // 해당 시트의 행 개수
     //source file의 row마다 순회
-    for (int rowIdx = 1; rowIdx <= rows; rowIdx++) {
+    for (int rowIdx = 0; rowIdx <= rows; rowIdx++) {
       XSSFRow row = sheet.getRow(rowIdx);
       FileInputStream template = new FileInputStream(templateFilename);
       XSSFWorkbook templateWorkbook = new XSSFWorkbook(template);
@@ -57,31 +59,62 @@ public class XlsxService {
         int secondItemVal= (int)(secondItem.getNumericCellValue());
         System.out.println("Date : "+firstItemVal+", Cost : "+secondItemVal);
 
-
-        XSSFSheet sheet1 = templateWorkbook.getSheetAt(0);
-        int rowCount = 8;
-        XSSFRow templateRow = sheet1.getRow(rowCount);
-        XSSFCell templateRowCell = templateRow.getCell(5);
-        while(templateRow.getCell(5).getCellType()!=XSSFCell.CELL_TYPE_BLANK){
-          rowCount++;
+        XSSFSheet sheet1 = templateWorkbook.getSheetAt(1);
+        sheet1.getRow(3).getCell(1).setCellValue(convertDateStringToDate(firstItemVal));
+        sheet1.getRow(3).getCell(2).setCellFormula("B4");
+        XSSFRow templateRow= sheet1.getRow(1);
+        int rowCount=0;
+        int sheet1Rows = sheet1.getPhysicalNumberOfRows();
+        for(rowCount = 6; rowCount<=sheet1Rows;rowCount++){
           templateRow = sheet1.getRow(rowCount);
+          if(templateRow.getCell(4).getCellType()==XSSFCell.CELL_TYPE_BLANK){
+            break;
+          }else if (templateRow.getCell(4).getCellType()==XSSFCell.CELL_TYPE_FORMULA){
+            templateRow.getCell(4).setCellFormula(templateRow.getCell(4).getCellFormula());
+          }
         }
-        System.out.println(rowCount+"번째 행");
-        XSSFCell cell1 = templateRow.createCell(5);
-        XSSFCell cell2 = templateRow.createCell(10);
+        XSSFCell cell1 = templateRow.createCell(4);
+        XSSFCell cell2 = templateRow.createCell(9);
         cell1.setCellType(Cell.CELL_TYPE_STRING);
         cell2.setCellType(Cell.CELL_TYPE_NUMERIC);
-        cell1.setCellValue(firstItemVal);
+        cell1.setCellFormula("TEXT($B$4,\"MM월 DD일\")");
         cell2.setCellValue(secondItemVal);
 
         File outputDir = new File(attach_path+output_path);
         if(!outputDir.exists()){
           outputDir.mkdirs();
         }
-        FileOutputStream fileOut = new FileOutputStream(outputDir+"/After"+rowIdx+".xlsx");
+        FileOutputStream fileOut = new FileOutputStream(outputDir+"/"+firstItemVal+".xlsx");
         templateWorkbook.write(fileOut);
         fileOut.close();
       }
     }
+  }
+  public String convertDateStr(String item){
+    String[] split = item.split("\\.");
+    String year = split[0];
+    String month = split[1];
+    String day = split[2];
+    StringBuilder stringBuilder = new StringBuilder();
+    StringBuilder append = stringBuilder.append(month).append("월").append(" ").append(day).append("일");
+    return append.toString();
+  }
+  public String convertDateWithSlash(String item){
+    String[] split = item.split("\\.");
+    String year = split[0];
+    String month = split[1];
+    String day = split[2];
+    StringBuilder stringBuilder = new StringBuilder();
+    StringBuilder append = stringBuilder.append(month).append("/").append(day).append("/").append(year);
+    return append.toString();
+  }
+  public Calendar convertDateStringToDate(String item){
+    String[] split = item.split("\\.");
+    String year = split[0];
+    String month = split[1];
+    String day = split[2];
+    Calendar date = Calendar.getInstance();
+    date.set(Integer.parseInt(year),Integer.parseInt(month)-1,Integer.parseInt(day));
+    return date;
   }
 }
